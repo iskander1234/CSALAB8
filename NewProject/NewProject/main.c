cc2b4407b8c05373e9501c87cdca3bcec7a65665
Iskander Dosgali <idosgali@gmail.com>
2021-04-27 13:17:02 +0600

    .

--- NewProject/NewProject/main.c
+++ NewProject/NewProject/main.c
@@ -2,71 +2,104 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-void lcdInitPin(int registerNumber){
+void lcd_init_pin(int register_number){
 	PORTB = 0x02;
 	_delay_ms(100);
-	PORTA = registerNumber;
+	PORTA = register_number;
 	PORTB = 0x00;
 	_delay_ms(100);
 }
 
-//void lcd_s1 (int register_number)
-//{
-	//PORTE = 0x02;
-	//_delay_ms (100)
-	//PORTD = register_number;
-	//PORTE = 0x00;
-//	_delay_ms (100)
-//}
 
-
-void lcdDisplayStr(char *str){
-	lcdInitPin(0x30);
-	lcdInitPin(0x0E);
-	lcdInitPin(0x80);
+void lcd_display_str1(char *str){
+	lcd_init_pin(0x38);
+	lcd_init_pin(0x0E);
+	lcd_init_pin(0x80);
 	
-	for(int i=0; str[i] != '\0'; i++)
-	{
-		lcdDisplayChar(str[i]);
+	for(int i =0; str[i] != '\0'; i++){
+		lcd_display_char(str[i]);
 	}
+	
 }
 
-//void lcd_display_str2 (char * str1)
-//{
-//	lcd_s1 (0x38)
-//	lcd_s1 (0x0E)
-//	lcd_s1 (0xC0)
+void lcd_display_str2(char *str){
+	lcd_init_pin(0x38);
+	lcd_init_pin(0x0E);
+	lcd_init_pin(0xC0);
 	
-//	for (int a = 0; str1 [a]! = '\0'; a ++)
-//	{
-//		lcd_char(str1 [a])
-//	}
-//}
-
-//void lcd_char (char * character)
-//{
-//	PORTB = 0x03;
-//	PORTA = character;
-//	_delay_ms (100)
-//	PORTB = 0x01;
-//}
-
-void lcdDisplayChar(char *charcter){
+	for(int i =0; str[i] != '\0'; i++){
+		lcd_display_char(str[i]);
+	}
+	
+}
+void lcd_display_char(char *character){
 	PORTB = 0x03;
-	PORTA = charcter;
+	PORTA = character;
 	_delay_ms(50);
 	PORTB = 0x01;
 }
+
+
+
+void lcd_init_pin2(int register_number){
+	PORTF= 0x02;
+	_delay_ms(100);
+	PORTE= register_number;
+	PORTF= 0x00;
+	_delay_ms(100);
+}
+
+void lcd_display_char2(char *character){
+	PORTF= 0x03;
+	PORTE= character;
+	_delay_ms(50);
+	PORTF= 0x01;
+}
+
+void lcd_display_str3(char *str){
+	lcd_init_pin2(0x38);
+	lcd_init_pin2(0x0E);
+	lcd_init_pin2(0x80);
+	
+	for(int i =0; str[i] != '\0'; i++){
+		lcd_display_char2(str[i]);
+	}
+	
+}
+
+void lcd_display_str4(char *str){
+	lcd_init_pin2(0x38);
+	lcd_init_pin2(0x0E);
+	lcd_init_pin2(0xC0);
+	for(int i =0; str[i] != '\0'; i++){
+		lcd_display_char2(str[i]);
+	}
+}
+
+void lcd_display_str6( char *str){
+	lcd_init_pin2(0x38);
+	lcd_init_pin2(0x0E);
+	lcd_init_pin2(0xC0);
+		for(int i =0; str[i] != '\0'; i++){
+			lcd_display_char2(str[i]);
+		}
+}
+
 
 int main(void)
 {
 	DDRA = 0xFF;
 	DDRB = 0xFF;
-	DDRD = 0xFF;
-	DDRE = 0xFF;
 	
-	lcdDisplayStr("DOSGALI ISKANDER");
+	lcd_display_str1("Iskander");
+	lcd_display_str2("Dosgali");
+	
+	DDRE =  0xFF;
+	DDRF = 0xFF;
+	
+	lcd_display_str3("Iskander");
+	//lcd_display_str4("Dosgali");
+	//lcd_display_str5("Dosgaliuly");
+	lcd_display_str6("IS-1801k");
+
 }
-
-
